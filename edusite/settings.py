a7d8641d90605b0c38620c87bd9b81267df0ce00
great-special <sirgreatspecial@gmail.com>
2025-01-21 21:48:04 +0100

    added langauges

--- edusite/settings.py
+++ edusite/settings.py
@@ -134,6 +134,8 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
