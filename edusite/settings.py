31b4377cabc440bd747323c6a35173f3b00e5e4c
great-special <sirgreatspecial@gmail.com>
2025-02-14 01:48:56 +0100

    now ready

--- edusite/settings.py
+++ edusite/settings.py
@@ -86,18 +86,18 @@ WSGI_APPLICATION = 'edusite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    # 'default':{
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'icitjssa_citadel',
-    #     'USER': 'icitjssa_dikachi',
-    #     'PASSWORD': 'eRgsxCK5w&eE',
-    #     'HOST': 'localhost',
-    #     'PORT': '3306',
-    # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'icitjssa_citadel',
+        'USER': 'icitjssa_dikachi',
+        'PASSWORD': 'eRgsxCK5w&eE',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
 # eRgsxCK5w&eE
@@ -153,8 +153,8 @@ MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-# STATIC_ROOT =  '/home/icitjssa/public_html/static'
+# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT =  '/home/icitjssa/public_html/static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
